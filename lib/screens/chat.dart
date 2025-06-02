4c647ba29ea3e1d8e4b0dc890946db05a43a5f25
Tamby Tia RAZAFINDRALAMBO <tambytia.razafindralambo@elca.mu>
2025-06-02 11:08:19 +0400

    add notification

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,30 @@
 import 'package:demo7/widget/chat_messages.dart';
 import 'package:demo7/widget/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  void setupPushNotif() async {
+
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotif();
+  }
 
   @override
   Widget build(BuildContext context) {
